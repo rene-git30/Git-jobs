3db64db71ac42f75fadd1eb0745ec363773373d9
Rene Ariza <jenryrenea@gmail.com>
2024-12-16 21:31:46 -0500

    new file:   programa1.py

---
+++ programa1.py
@@ -0,0 +1,7 @@
+arreglo=[[1,2,3,4],[5,6,7,8]]
+
+for i in range (len(arreglo)):
+    for j in range (len(arreglo[i])):
+        print(arreglo[i][j], end=' ')
+    print()
+print("len_arreglo[i]",len(arreglo[i]))
